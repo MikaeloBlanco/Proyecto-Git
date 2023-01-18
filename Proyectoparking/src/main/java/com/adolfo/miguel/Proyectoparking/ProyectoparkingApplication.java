3d997111236cf126c94b3bbccb5806d72ba6f5fc
Miguel <mblafer963@g.educaand.es>
2023-01-18 19:52:27 +0100

    Primer commit

---
+++ Proyectoparking/src/main/java/com/adolfo/miguel/Proyectoparking/ProyectoparkingApplication.java
@@ -0,0 +1,13 @@
+package com.adolfo.miguel.Proyectoparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoparkingApplication.class, args);
+	}
+
+}
---
+++ Proyectoparking/src/test/java/com/adolfo/miguel/Proyectoparking/ProyectoparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adolfo.miguel.Proyectoparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
