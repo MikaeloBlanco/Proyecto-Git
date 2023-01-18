daee532001d465b03fdd4b48ac6ee50c943e8112
Miguel <mblafer963@g.educaand.es>
2023-01-18 20:49:39 +0100

    commit "registro usuarios"

---
+++ Proyectoparking/src/main/java/user/user.java
@@ -0,0 +1,29 @@
+public class user {
+
+
+    private long id;
+    private String email;
+    private String nombre;
+    
+    public User(String email, String nombre){
+        this.email = email;
+        this.nombre = nombre;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getNombre{
+        return this.nombre;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    } 
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+}
---
+++ Proyectoparking/src/main/java/user/userRepository.java
@@ -0,0 +1,18 @@
+package user;
+
+public class userRepository {
+    
+
+    public List<user> getAll(){
+
+        //TODO : Eliminar este codigo a continuación:
+        ArrayList<user> users = new ArrayList<>();
+        users.add(new User(email: "miguel", nombre: "mblafer963@g.educaand.es"));
+        return users;
+
+
+
+    }
+
+
+}
